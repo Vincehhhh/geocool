9c52e7cb5fafe449061c3f3db00bb274ea756a5d
clara pecot <clara.pecot44@gmail.com>
2023-06-08 15:39:50 +0200

    nothing done

--- app/controllers/working_well_systems_controller.rb
+++ app/controllers/working_well_systems_controller.rb
@@ -1,9 +1,9 @@
 class WorkingWellSystemsController < ApplicationController
 
   def index
-    @results = GeoCoolSolver.compute("result_from_form")
-    @working_well = results.first
-    @working_well.new()
+    # @results = GeoCoolSolver.compute("result_from_form")
+    # @working_well = results.first
+    # @working_well.new()
   end
 
   def show
