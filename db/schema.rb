3cfedf1b11a0a32aceab8fe6d9a333949c403fee
Almis Lipon <almislpn@gmail.com>
2023-06-14 10:54:17 +0200

    Seed Ground.details/ install pip

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_124221) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_080626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_124221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "source"
+    t.text "details"
   end
 
   create_table "guides", force: :cascade do |t|
